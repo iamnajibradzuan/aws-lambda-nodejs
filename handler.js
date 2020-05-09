62b1a3721ab87de7f5b5da4166f9170cfb4023e0
Najib Radzuan <anmrv@lemu.dk>
2020-05-09 22:52:19 +0800

    finished

--- handler.js
+++ handler.js
@@ -5,14 +5,21 @@ module.exports.hello = async event => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+        message: 'This is edited code function code',
+      }),
   };
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
 
+module.exports.imageResizer = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'This is imageResizer code',
+      }),
+  };
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
